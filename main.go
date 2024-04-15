a961d82413c688b50485cf1bfcf249b9ab8c6685
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-15 20:56:01 +0600

    Refactor main.go to use fmt.Println for server startup message and update API server port

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	api := NewAPIServer(":8080")
+	fmt.Println(
+		`
+	_    _    __  __     __  __         _   
+	| |  | |  |  \/  |___|  \/  |___  __| |__
+	| |__| |__| |\/| |___| |\/| / _ \/ _| / /
+	|____|____|_|  |_|   |_|  |_\___/\__|_\_\
+	`)
+	api := NewAPIServer(":6969")
 	api.Run()
 }
